0ea823496c3ba6b473ada4f8f14d856e65f74c61
Rakin Rahman <rakinhacker@gmail.com>
2026-01-19 17:42:24 +0600

    feat: scrape json data

--- writer/faculty_scraper.py
+++ writer/faculty_scraper.py
@@ -2,7 +2,6 @@ from typing import final
 from dataclasses import dataclass
 
 import requests
-from bs4 import BeautifulSoup
 
 from .constants import FACULTIES_URL
 from .utils import removeExtraWhitespaces
@@ -16,24 +15,19 @@ class Teacher:
 
 @final
 class FacultyScraper:
-    # FIX: Name and image URL does not load. Use selenium instead of requests and bs4.
     def __init__(self):
-        page = requests.get(FACULTIES_URL)
-        self.soup = BeautifulSoup(page.content, "lxml")
-        print(self.soup)
+        response = requests.get(FACULTIES_URL)
+        self.data = response.json()
 
     def get_teachers(self) -> list[Teacher]:
         teachers: list[Teacher] = []
-        profiles = self.soup.find_all(
-            "img", {":src": "profile.PersonalOtherInfo.SecondProfilePhoto"}
-        )
+        profiles = self.data["EmployeeProfileLightList"]
 
         for profile in profiles:
-            name = str(profile.get("alt", "Unknown"))
+            name: str = profile["CvPersonal"]["Name"]
             name = removeExtraWhitespaces(name).title()
 
-            # TODO: Set backup image.
-            image_path = str(profile.get("src"))
+            image_path: str = profile["PersonalOtherInfo"]["SecondProfilePhoto"]
             image_url = "https://www.aiub.edu" + image_path
 
             teachers.append(Teacher(name, image_url))
