7c291e1eb8244dd415f8b2c89f16a5b0055cffa7
Rakin Rahman <rakinhacker@gmail.com>
2026-01-19 18:24:29 +0600

    feat: view image

--- writer/gui.py
+++ writer/gui.py
@@ -2,21 +2,24 @@ from typing import final
 import sys
 
 from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt6.QtGui import QPixmap
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.setWindowTitle("Faculty Writer")
+        self.setFixedSize(QSize(900, 800))
 
-        self.setWindowTitle("My App")
+        label = QLabel(self)
+        pixmap = QPixmap("/home/rakin/Pictures/rakin.jpeg")
+        pixmap = pixmap.scaledToHeight(self.height())
+        label.setPixmap(pixmap)
+        label.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-        button = QPushButton("Press Me!")
-
-        self.setFixedSize(QSize(400, 300))
-
-        # Set the central widget of the Window.
-        self.setCentralWidget(button)
+        # Set the central widget of the window
+        self.setCentralWidget(label)
 
 
 @final
